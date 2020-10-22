46d393084040007a9e1b90acdd25727f12e884bf
Daniel Mayusa Dewa <Danielmayusadewa@gmail.com>
2020-10-22 09:20:18 +0700

    Update config.local.js

--- server/config.local.js
+++ server/config.local.js
@@ -6,5 +6,5 @@ const version = p.version.split('.').shift();
 module.exports = {
   restApiRoot: '/api' + (version > 0 ? '/v' + version : ''),
   host: '0.0.0.0',
-  port: process.env.KOOL_PORT || 3000,
+  port: process.env.KOOL_PORT || 8000,
 };
