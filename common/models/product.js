569ac07b037c43c36cd79c65dc6f741e5c26d109
Daniel Mayusa Dewa <Danielmayusadewa@gmail.com>
2020-11-02 12:54:30 +0700

    update Product

--- common/models/product.js
+++ common/models/product.js
@@ -1,5 +1,19 @@
 'use strict';
 
-module.exports = function(Ingredient) {
-
+module.exports = function(Product) {
+    Product.getMyItem = async () => {
+        try {
+            if (req.accessToken == null) {
+            throw errorHelper.badAuthorization();
+            }
+          const product = await Product.findById(req.accessToken.userId);
+          return Promise.resolve({
+            message: "success",
+            status: "success",
+            product
+          });
+        } catch (error) {
+            throw err;
+        }
+    }
 };
